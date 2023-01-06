245c8c0e7c1e23a0dad9cc296bfb5f3a2ef6b76e
Saharuddin Azman <36906104+d4rkpack3t@users.noreply.github.com>
2023-01-06 18:04:04 +0800

    update_saha

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -9,8 +9,8 @@
         @endauth
 
         @guest
-        <h1>Homepage</h1>
-        <p class="lead">Your viewing the home page. Please login to view the restricted data.</p>
+        <h1>NETWORK SECURITY DEVELOPMENT CENTRE​</h1>
+        <p class="lead">A company that implements Information Security Management System (ISMS) and focuses on becoming Malaysia’s First Network Security Development Center. We provide diverse solution including Training & Workshop, Knowledge Sharing & Technology Updates, Network Assessment, Application Assessment, Security Assessment, Cyber Defense & Offense Simulation Range, Network Visibility & Monitoring Solutions and Training Facilities to a variety of customers from government and private sectors.</p>
         @endguest
     </div>
 @endsection
