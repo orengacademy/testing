3973401d7bec6e43b6bae8ce83088b6049fe3aac
learzahaker <daniel@orengacademy.com>
2023-01-06 23:37:11 +0800

    Update profile.blade.php

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -1,33 +1,40 @@
 @extends('layouts.app-master')
 
 @section('content')
-<form method="post" action="{{ route('login.perform') }}">
+    <div class="bg-light p-5 rounded">
+        @auth
 
-    <input type="hidden" name="_token" value="{{ csrf_token() }}" />
-    <!-- <img class="mb-4" src="{!! url('images/OrenG-Logo-3-7-768x276.svg') !!}" alt="" width="72" height="57"> -->
 
-    <h1 class="h3 mb-3 fw-normal">Login</h1>
+        <form method="post" action="{{ route('login.perform') }}">
 
-    @include('layouts.partials.messages')
+            <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+            <!-- <img class="mb-4" src="{!! url('images/OrenG-Logo-3-7-768x276.svg') !!}" alt="" width="72" height="57"> -->
+    
+            <h1 class="h3 mb-3 fw-normal">Profile Pages</h1>
+    
+            @include('layouts.partials.messages')
+    
+            <div class="form-group form-floating mb-3">
+                <input type="text" class="form-control" name="username" value="{{ old('username') }}" placeholder="Username" required="required" autofocus>
+                <label for="floatingName">Email or Username</label>
+                @if ($errors->has('username'))
+                    <span class="text-danger text-left">{{ $errors->first('username') }}</span>
+                @endif
+            </div>
+    
+            <div class="form-group form-floating mb-3">
+                <input type="password" class="form-control" name="password" value="{{ old('password') }}" placeholder="Password" required="required">
+                <label for="floatingPassword">Password</label>
+                @if ($errors->has('password'))
+                    <span class="text-danger text-left">{{ $errors->first('password') }}</span>
+                @endif
+            </div>
+    
+            <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
+    
+            @include('auth.partials.copy')
+        </form>
 
-    <div class="form-group form-floating mb-3">
-        <input type="text" class="form-control" name="username" value="{{ old('username') }}" placeholder="Username" required="required" autofocus>
-        <label for="floatingName">Email or Username</label>
-        @if ($errors->has('username'))
-            <span class="text-danger text-left">{{ $errors->first('username') }}</span>
-        @endif
+        @endauth
     </div>
-
-    <div class="form-group form-floating mb-3">
-        <input type="password" class="form-control" name="password" value="{{ old('password') }}" placeholder="Password" required="required">
-        <label for="floatingPassword">Password</label>
-        @if ($errors->has('password'))
-            <span class="text-danger text-left">{{ $errors->first('password') }}</span>
-        @endif
-    </div>
-
-    <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
-
-    @include('auth.partials.copy')
-</form>
 @endsection
