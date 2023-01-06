aecc7acb961f196e88470cd72ec0e354461ce121
Saharuddin Azman <36906104+d4rkpack3t@users.noreply.github.com>
2023-01-06 17:48:04 +0800

    saha_update

---
+++ database/migrations/2023_01_06_094626_create_project_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+            Schema::create('projects', function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->timestamps();
+                $table->string('client');
+                $table->string('PMO');
+                $table->string('Engineers');
+                // $table->decimal('value', 8, 2);
+            });
+        }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
