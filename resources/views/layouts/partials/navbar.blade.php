a6d1465491bb756129a5ca42ce7be226e5a820d0
Saharuddin Azman <36906104+d4rkpack3t@users.noreply.github.com>
2023-01-06 19:04:01 +0800

    Update navbar.blade.php

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -5,6 +5,13 @@
         <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
       </a>
 
+      @guest
+      <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+
+      </ul>
+      @endguest
+
+
       @auth
       <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
         <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
