700f37812809a5dbaf3cd814922925afb4699001
learzahaker <daniel@orengacademy.com>
2023-01-06 18:10:32 +0800

    update user

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -25,8 +25,9 @@ class RegisterRequest extends FormRequest
     {
         return [
             'email' => 'required|email:rfc,dns|unique:users,email',
-            'username' => 'required|unique:users,username',
+            'name' => 'required|unique:users,name',
             'password' => 'required|min:8',
+            'role' => 'required',
             'password_confirmation' => 'required|same:password'
         ];
     }
