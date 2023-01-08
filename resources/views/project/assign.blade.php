994830a4396e8e9a60f8ad1b5df3bfed9c4ac4e1
Saharuddin Azman <36906104+d4rkpack3t@users.noreply.github.com>
2023-01-08 14:25:34 +0800

    update

--- resources/views/project/assign.blade.php
+++ resources/views/project/assign.blade.php
@@ -8,59 +8,26 @@
     <div class="bg-light p-5 rounded">
         @auth
 
-        <form action="{{ url('project/' .$project->id) }}" method="post">
+        <form action="{{ url('project') }}" method="post">
 
             <input type="hidden" name="_token" value="{{ csrf_token() }}" />
             <!-- <img class="mb-4" src="{!! url('images/OrenG-Logo-3-7-768x276.svg') !!}" alt="" width="72" height="57"> -->
 
-            <h1 class="h3 mb-3 fw-normal">New Project</h1>
+            <h1 class="h3 mb-3 fw-normal">Assign Engineers</h1>
 
             @include('layouts.partials.messages')
 
             <div class = "form-group row g-3" >
-            <div class="col-md-6 form-floating">
-                <input type="text" class="form-control" name="titleproject" id="titleproject" value="{{ old('username') }}" placeholder="Username" required="required" autofocus>
-                <label for="floatingName">Project Title</label>
-                @if ($errors->has('username'))
-                    <span class="text-danger text-left">{{ $errors->first('username') }}</span>
-                @endif
-            </div>
-
-            <div class="col-md-6 form-floating">
-                <input type="text" class="form-control" name="client" id="client" value="{{ old('password') }}" placeholder="Password" required="required">
-                <label for="floatingPassword">Client/Agency</label>
-                @if ($errors->has('password'))
-                    <span class="text-danger text-left">{{ $errors->first('password') }}</span>
-                @endif
-            </div>
-
-            <div class="col-md-6 form-floating">
-                <input type="text" class="form-control" name="projectcode" id="projectcode" value="{{ old('password') }}" placeholder="Password" required="required">
-                <label for="floatingPassword">Project Code</label>
-                @if ($errors->has('password'))
-                    <span class="text-danger text-left">{{ $errors->first('password') }}</span>
-                @endif
-            </div>
-
-            <div class="col-md-6 form-floating">
-                <input type="text" class="form-control" name="year" id="year" value="{{ old('password') }}" placeholder="Password" required="required">
-                <label for="floatingPassword">Year</label>
-                @if ($errors->has('password'))
-                    <span class="text-danger text-left">{{ $errors->first('password') }}</span>
-                @endif
-            </div>
 
 
           <div class="col-md-6 form-floating">
             <div class="form-group">
-          <label for="sel1">Project Manager (PMO)</label>
+          <label for="sel1">Client</label>
           <select class="form-control" name="PMO" id="PMO">
-            <option>Intan Harnila</option>
-            <option>Fyka</option>
-            <option>Rafidah</option>
-            <option>Amira</option>
-            <option>Zarihan</option>
+            @foreach($project as $item)
+            <option>{{ url($project->client) }}</option>
           </select>
+          @endforeach
         </div>
         </div>
 
