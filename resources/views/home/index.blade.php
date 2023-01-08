fe87387aac889633b83c958012d38f84796ee927
Saharuddin Azman <36906104+d4rkpack3t@users.noreply.github.com>
2023-01-08 12:18:24 +0800

    update

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,7 +4,7 @@
 
 <br><br>
 <div class="card mb-3">
-<img src="/images/Logo-OrenG.png" class="card-img-top" alt="...">
+<img src="{{ URL('storage/Logo-OrenG.png') }}" class="card-img-top" alt="...">
 <div class="card-body">
   <h5 class="card-title">Card title</h5>
   <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
